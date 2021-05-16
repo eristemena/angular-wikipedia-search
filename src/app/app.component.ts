f7fd7a4ac9292af336e83457117fbe79bc6a65a5
Eris Ristemena <e.ristemena@gmail.com>
2021-05-17 03:25:35 +0700

    Search & style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WikipediaService } from './wikipedia.service';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  pages = [];
+
+  constructor(private wikipediaService: WikipediaService) {}
+
   onSubmit(term: string) {
-    console.log(term);
+    this.wikipediaService.search(term)
+      .subscribe((response: any) => {
+        this.pages = response.query.search;
+      })
   }
 }
